0f2c89eae4a8f02785fa79744640a4ced3fb33aa
casonbobo <casonsbobo@gmail.com>
2023-05-09 13:16:52 -0700

    fixed my spelling mistake :/

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     mod = number % 10
 print(f"Last digit of {number} is {mod}", end=' ')
 if(mod > 5):
-    print("and is greater then 5")
+    print("and is greater than 5")
 elif(mod == 0):
     print("and is 0")
 else:
-    print("and is less then 6 and not 0")
+    print("and is less than 6 and not 0")
