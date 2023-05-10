1cde19394b195ba1cd830ad718eb9ebc7cfd14a5
casonbobo <casonsbobo@gmail.com>
2023-05-10 12:17:35 -0700

    print my ass

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,10 +3,7 @@
 def print_last_digit(number):
     lastDigit = 0
 
-    if number < 0:
-        lastDigit = number % -10
-    else:
-        lastDigit = number % 10
+    lastDigit = number % 10
     
     print(lastDigit, end='')
     return(lastDigit)
