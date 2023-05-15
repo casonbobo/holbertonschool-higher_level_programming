4852e323e094e0585f051d78becc6ae0502f708e
casonbobo <casonsbobo@gmail.com>
2023-05-15 11:01:47 -0700

    do not speak ill of chet

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        last_element = row[-1]
         for element in row:
-            if row != len(row):
-                print("{:d}".format(element), end=' ')
+            if element != last_element:
+                print("{:d} ".format(element), end='')
             else:
-                print("{:d}".format(element))
+                print("{:d}".format(element), end='$')
         print()
