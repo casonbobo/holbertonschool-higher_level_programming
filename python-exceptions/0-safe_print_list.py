a16b50ebfbadb674eb88e1849a9e49ec3a61d704
casonbobo <casonsbobo@gmail.com>
2023-05-16 11:29:32 -0700

    maybe maybe maybe plz dear god

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     for x in range(x):
         try:
-            print(my_list[x])
+            print(my_list[x], end='')
             count +=1
         except:
             return count
-
     return count
