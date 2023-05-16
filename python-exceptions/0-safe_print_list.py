a9f42a6e5a89037a25b2fa05dff16ef55e98c826
casonbobo <casonsbobo@gmail.com>
2023-05-16 12:23:39 -0700

    this is realy annoying

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for x in range(x):
         try:
             print(my_list[x], end='')
-            count += 1
+            count +=1
         except:
-            break
+            return count
 
-    print()
     return count
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except:
             continue
         else:
-        count += 1
+            count += 1
     print()
-    return ,count
+    return count
