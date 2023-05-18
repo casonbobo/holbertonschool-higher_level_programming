d0fafdd47336580e25e6f1766f9348baa456fb48
casonbobo <casonsbobo@gmail.com>
2023-05-18 12:45:28 -0700

    plz caramon

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,9 +13,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self, size):
-        self.area == size**2
-        return(self.area)
+    def area(self):
+        return(self.__size * self.__self)
     
     def size(self):
         return(self.__size)
