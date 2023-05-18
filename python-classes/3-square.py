041565b6332324c5d30dc3cfca707446c6dacacd
casonbobo <casonsbobo@gmail.com>
2023-05-18 11:44:11 -0700

    added a 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+square
+"""
+
+
+class Square:
+    """this class defines a square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self, size):
+        self.area = size * size
+    pass
