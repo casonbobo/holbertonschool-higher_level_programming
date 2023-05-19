418b2e92c417906020cd8d090050f8ef780b9c95
casonbobo <casonsbobo@gmail.com>
2023-05-19 10:37:38 -0700

    next set of changes. who knows when itll work through

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,8 @@ square
 class Square:
     """this class defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return(self.__size * self.__size)
