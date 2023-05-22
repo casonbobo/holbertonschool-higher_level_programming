68ba91930b1380b2be8327792424274322f1fadc
casonbobo <casonsbobo@gmail.com>
2023-05-22 12:37:46 -0700

    why does this not work >:(

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -38,6 +38,6 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        if self.width or self.height == 0:
+        if self.width == 0 or self.height == 0:
             return(0)
         return (self.height * 2) + (self.width * 2)
