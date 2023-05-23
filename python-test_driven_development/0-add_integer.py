71e2e0e6f63f54097ff340c0824a8108d4967dba
casonbobo <casonsbobo@gmail.com>
2023-05-23 12:59:55 -0700

    ugh please

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,9 +7,10 @@ def add_integer(a, b=-98):
     a function that adds integers or casts 
     floats to integers and adds them
     """
-    if a is not type(float):
+    if a is None or type(a) not in [int, float] or abs(a) > float('inf'):
         raise TypeError("a must be an integer")
-    elif b is not type(float):
+    if b is None or type(b) not in [int, float] or abs(b) > float('inf'):
         raise TypeError("b must be an integer")
-    else:
-        return(int(a + b))
+    a = int(a)
+    b = int(b)
+    return a + b
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""
+takes a matrix and divides it by a passed number
+"""
 def matrix_divided(matrix, div):
+    """
+    OoooOOoo
+    the matrix
+    """
     if (isinstance(matrix, list) and all(isinstance(row, list)
         and all(isinstance(element, int) for element in row) for row in matrix)):
         for row in matrix:
