683906810a6b0b30725e150ab0ac4b485340e739
casonbobo <casonsbobo@gmail.com>
2023-05-23 12:54:25 -0700

    ugh documentaion

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-""" Adding Integers"""
+"""
+Adding Integers
+"""
 def add_integer(a, b=-98):
+    """
+    a function that adds integers or casts 
+    floats to integers and adds them
+    """
     if a is not type(float):
         raise TypeError("a must be an integer")
     elif b is not type(float):
