dfb55ae446a70cceb74fdb6bca7da1feba5a91a5
casonbobo <casonsbobo@gmail.com>
2023-05-26 10:28:44 -0700

    eucliding distance obvi

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-
+this is a funuction to sort a list without altering the list
 """
 
 
 class MyList(list):
+    """ this is the class. duh"""
     def print_sorted(self):
         print(sorted(self))
