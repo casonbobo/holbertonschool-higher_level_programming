087924df2ee9c1ad57313c6cd3a7871238c1ef41
casonbobo <casonsbobo@gmail.com>
2023-05-26 12:30:47 -0700

    forgot a name again

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@ checks to see if these objects are exactly the same, or kind of the same
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """this is the function"""
     return (isinstance(obj, a_class))
