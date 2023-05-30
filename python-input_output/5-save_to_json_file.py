d6a1ff32f08d9d78e11a1c37978b1ad6ebec265f
casonbobo <casonsbobo@gmail.com>
2023-05-30 12:52:58 -0700

    added 6 and changed some shiz

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a file to write other json"""
+import json
+
+
+def save_to_json_file (my_obj, filename):
+    """this is a function to append to a json"""
+    with open(filename, mode='w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
