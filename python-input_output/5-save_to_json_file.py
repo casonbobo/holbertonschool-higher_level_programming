b97699a2942890a14b0878368e0c3ee5f6458ca2
casonbobo <casonsbobo@gmail.com>
2023-05-30 13:45:07 -0700

    hot dog homieo stasis

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def save_to_json_file (my_obj, filename):
+def save_to_json_file(my_obj, filename):
     """this is a function to append to a json"""
     with open(filename, mode='w', encoding="utf-8") as f:
         json.dump(my_obj, f)
