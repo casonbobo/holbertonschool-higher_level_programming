25150185de7c5e89f600c429c2d5e2331539f5c1
casonbobo <casonsbobo@gmail.com>
2023-05-30 13:56:54 -0700

    just tired and depressed

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def save_to_json_file (my_obj, filename):
+def load_from_json_file(my_obj, filename):
     """this is a function to append to a json"""
     with open(filename, mode='r', encoding="utf-8") as f:
         return json.load(f)
