909366e9c40099194ef1f72ef6569d961247e925
casonbobo <casonsbobo@gmail.com>
2023-06-01 14:27:51 -0700

    added super duh

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,31 +8,28 @@ from models.base import Base
 class Rectangle(Base):
     """This is a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        if type(width) is not int or type(height) is not int:
-            raise TypeError("size must be an integer")
-        if width < 0 or height < 0:
-            raise ValueError("size must be >= 0")
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-    
+
     @property
     def width(self):
         return(self.__width)
-    
+
     @property
     def height(self):
         return(self.__height)
-    
+
     @property
     def x(self):
         return(self.__x)
-    
+
     @property
     def y(self):
         return(self.__y)
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -56,7 +53,7 @@ class Rectangle(Base):
         if value < 0:
                 raise ValueError("size must be >= 0")
         self.__x = value
-    
+
     @y.setter
     def y(self, value):
         if type(value) is not int:
@@ -68,4 +65,3 @@ class Rectangle(Base):
     def area(self):
         """Area function for rectangle"""
         return(self.__height * self.__height)
-
