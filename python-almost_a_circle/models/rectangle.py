03f6a4429ef3fd9b2896f569f0a0d44d24268e81
casonbobo <casonsbobo@gmail.com>
2023-06-01 15:04:22 -0700

    fixed my text calls

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,33 +33,33 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @width.setter
     def width(self, value):
         if type(value) is not int:
-                raise TypeError("size must be an integer")
+                raise TypeError("width must be an integer")
         if value < 0:
-                raise ValueError("size must be >= 0")
+                raise ValueError("width must be > 0")
         self.__width = value
 
     @x.setter
     def x(self, value):
         if type(value) is not int:
-                raise TypeError("size must be an integer")
+                raise TypeError("x must be an integer")
         if value < 0:
-                raise ValueError("size must be >= 0")
+                raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         if type(value) is not int:
-                raise TypeError("size must be an integer")
+                raise TypeError("y must be an integer")
         if value < 0:
-                raise ValueError("size must be >= 0")
+                raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
