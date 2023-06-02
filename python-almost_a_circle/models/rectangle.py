1a52a0241a7406d199cf4a7ad3584c067ebb419f
casonbobo <casonsbobo@gmail.com>
2023-06-02 10:07:29 -0700

    cleaned it up. Thanks chat

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,17 +82,10 @@ class Rectangle(Base):
                 .format(self.id, self.x, self.y, self.width, self.height))
 
     def update(self, *args, **kwargs):
-            """\_:|_/"""
-            if args:
-                if len(args) >= 1:
-                    self.id = args[0]
-                if len(args) >= 2:
-                    self.__width = args[1]
-                if len(args) >= 3:
-                    self.__height = args[2]
-                if len(args) >= 4:
-                    self.__x = args[3]
-                if len(args) >= 5:
-                    self.__y = args[4]
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        """\_:|_/"""
+        attribute = ["id", "width", "height", "x", "y"]
+        if args:
+            for attr, value in zip(attribute, args):
+                setattr(self, attr, value)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
