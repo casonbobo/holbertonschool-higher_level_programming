d404d0f26708ec75f1d8fb999df375d53bb2758f
casonbobo <casonsbobo@gmail.com>
2023-06-02 10:44:44 -0700

    maybe fixed square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,12 +5,11 @@ This is for a square. and not a circle
 from models.rectangle import Rectangle
 
 
-
 class Square(Rectangle):
     """This is a square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width = size, height = size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}"
-                .format(self.id, self.x, self.y, self.size))
+                .format(self.id, self.x, self.y, self.width or self.height))
