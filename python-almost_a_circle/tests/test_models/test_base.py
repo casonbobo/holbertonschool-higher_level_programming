b93918f7e31ac84b8052a029c53b31aabe765dd8
casonbobo <casonsbobo@gmail.com>
2023-06-06 11:26:39 -0700

    its fine. this is fien. everythings fine

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,67 +3,48 @@
 Unittest module for Base class.
 """
 import unittest
-from models.base import Base as Base
-from models.rectangle import Rectangle as Rectangle
-from models.square import Square as Square
+from models.base import Base
+from unittest.mock import patch
+from io import StringIO
 
 
-class BaseTests(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """
-    Tests for base class
+    class for testing Base class
     """
-    @classmethod
-    def setUpClass(cls):
-        """making the class' for testing"""
-        cls.base1 = Base()
-        cls.rectangle1 = Rectangle(1, 2, 0, 0, 100)
-        cls.square1 = Square(2, 0, 0, 200)
+    def test_auto_id_assign(self):
+        """Auto assign ID exists"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(99)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 99)
 
-    def delClass(cls):
-        """deletes the test class'"""
-        del cls.base1
-        del cls.rectangle1
-        del cls.square1
-        return super().delClass()
+    def test_id_type(self):
+        """test the type of id to ensure an integer"""
+        base = Base(99)
+        self.assertIsInstance(base.id, int)
 
-    def test_init(self):
-        """
-        Method for testing initialization of the base class
-        """
-        self.assertEqual(self.base1.id, 1)
+    def test_to_json_empty(self):
+        """test empty to json"""
+        with self.assertRaises(TypeError):
+            b1 = Base()
+            json_dictionary = Base.to_json_string()
 
-        self.base2 = Base(2)
-        self.assertEqual(self.base2.id, 2)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_to_json_string_with_none(self, mock_stdout):
+        expected_output = "[]"
+        result = Base.to_json_string(None)
+        self.assertEqual(result, expected_output)
+        self.assertEqual(mock_stdout.getvalue(), "")
 
-        self.base3 = Base()
-        self.assertEqual(self.base3.id, 2)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_from_json_string_with_none(self, mock_stdout):
+        expected_output = []
+        result = Base.from_json_string(None)
+        self.assertEqual(result, expected_output)
+        self.assertEqual(mock_stdout.getvalue(), "")
 
-        self.assertEqual(self.base2.id, self.base3.id)
-
-        del self.base2
-        del self.base3
-        
-    def test_to_json_string(self):
-        """
-        Method for testing the json string creation
-        """
-        dictionary_r = self.rect1.to_dictionary()
-        json_dictionary_r = self.rect1.to_json_string([dictionary_r])
-        dicStr = '[{"x": 0, "y": 0, "id": 100, "height": 1, "width": 2}]'
-        self.assertEqual(json_dictionary_r, dicStr)
-
-        dictionary_s = self.square1.to_dictionary()
-        json_dictionary_s = self.square1.to_json_string([dictionary_s])
-        dicStr = '[{"id": 200, "x": 0, "size": 2, "y": 0}]'
-        self.assertEqual(json_dictionary_s, dicStr)
-
-        self.assertRaises(TypeError, self.square1.to_json_string, dictionary_s)
-        self.assertRaises(TypeError, self.square1.to_json_string, [1])
-
-        dicList = [self.square1.to_dictionary(), self.rect1.to_dictionary()]
-        dicStr = '[{"id": 100, "x": 0, "size": 2, "y": 0}, ' \
-            + '{"x": 0, "y": 0, "id": 200, "height": 1, "width": 1}]'
-        self.assertEqual(self.square1.to_json_string(dicList), dicStr)
-
-        self.assertEqual(self.square1.to_json_string(None), "[]")
-        self.assertEqual(self.square1.to_json_string([]), "[]")
+if __name__ == '__main__':
+    unittest.main()
