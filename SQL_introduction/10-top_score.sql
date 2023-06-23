748e4f208b87ae3b7c61851b420ace26f7fec822
casonbobo <casonsbobo@gmail.com>
2023-06-23 11:31:04 -0700

    hopefully fixed now??

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- This is a comment
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- This is a comment
+DELETE FROM second_table WHERE score <= 5
