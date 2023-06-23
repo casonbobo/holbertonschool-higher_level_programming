21188c678eb5cde7f4d032566c06d5baa278fb87
casonbobo <casonsbobo@gmail.com>
2023-06-23 11:50:15 -0700

    this should be the last couple

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- This is not a comment
+SELECT AVG(score) FROM second_table;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- You think that of me
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC, name DESC;
