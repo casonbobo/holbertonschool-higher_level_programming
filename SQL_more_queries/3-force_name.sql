1507e4db19f9f39aeae5035d25921bab893411ab
casonbobo <casonsbobo@gmail.com>
2023-06-27 13:42:39 -0700

    these are the next two

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- This is not a comment
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- This is not a comment
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256) NOT NULL
+);
