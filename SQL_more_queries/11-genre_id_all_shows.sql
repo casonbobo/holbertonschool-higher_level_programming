3609bd740977f42addc24895856fe9be6041f0f4
casonbobo <casonsbobo@gmail.com>
2023-06-28 09:10:48 -0700

    forgot a s3mi

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- This is not a comment
 SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    ORDER BY tv_shows.title, tv_show_genres.show_id ASC
+    ORDER BY tv_shows.title, tv_show_genres.show_id ASC;
