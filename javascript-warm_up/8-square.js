fcdce8638e15828fd158d923c39796de359bdd2c
root <casonsbobo@gmail.com>
2023-07-27 14:49:52 -0700

    i made it a # instead of X

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const side = process.argv[2];
 
 for (let i = 0; i < side; i++) {
   for (let x = 0; x < side; x++) {
-    process.stdout.write('#');
+    process.stdout.write('X');
   }
   console.log('');
 }
