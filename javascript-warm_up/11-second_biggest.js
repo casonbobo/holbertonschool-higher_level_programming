9e7865b4be107f118dfc3a51e4d559bd7b9f502f
root <casonsbobo@gmail.com>
2023-07-28 11:44:31 -0700

    dayum it worked

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = process.argv.slice(2);
-num.sort ((a, b) => b - a);
+num.sort((a, b) => b - a);
 if (!(num) || !(num[1])) {
   console.log('0');
 } else {
---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+const updatedInfo = {
+  value: 89
+}
+const update = Object.assign({}, myObject, updatedInfo)
+console.log(update);
