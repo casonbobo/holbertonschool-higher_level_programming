4e7412868b691187e7ee3b28cfd8c5924678fbe1
root <casonsbobo@gmail.com>
2023-07-28 10:39:13 -0700

    finished 11 and fixed the others

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const num = process.argv.slice(2);
+num.sort ((a, b) => b - a);
+if (!(num) || !(num[1])) {
+  console.log('0');
+} else {
+  console.log(num[1]);
+}
--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const side = process.argv[2];
-if (side === Number) {
+if (!(isNaN(side))) {
   for (let i = 0; i < side; i++) {
     for (let x = 0; x < side; x++) {
       process.stdout.write('X');
     }
     console.log('');
   }
-}else{
-  console.log('Missing size')
+} else {
+  console.log('Missing size');
 }
