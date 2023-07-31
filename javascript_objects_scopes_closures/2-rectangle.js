40c5d97445c03749e9b0d90b8a68d55c9e4219d1
root <casonsbobo@gmail.com>
2023-07-31 11:09:56 -0700

    maybe now

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      process.exit(0);
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
+
 module.exports = Rectangle;
