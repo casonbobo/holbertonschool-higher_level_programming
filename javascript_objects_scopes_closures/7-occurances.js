124dd867a4497909c55e0811cd6bd20da7e0668c
root <casonsbobo@gmail.com>
2023-08-01 11:01:30 -0700

    7 and 8 from the comfort of my home

---
+++ javascript_objects_scopes_closures/7-occurances.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+  exports.nbOccurences = function (list, searchElement) {
+    const occurrences = {};
+    for (const item of list) {
+      occurrences[item] = occurrences[item] ? occurrences[item] + 1 : 1;
+    }
+    return occurrences;
+  }
+ 
