d8794204503fd1c5414b9ba054a8dd10574b7bcb
root <casonsbobo@gmail.com>
2023-08-01 11:42:06 -0700

    stupid

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const occurrences = {};
-  for (const item of list) {
-    occurrences[item] = occurrences[item] ? occurrences[item] + 1 : 1;
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
   }
-  return occurrences;
+  return count;
 };
