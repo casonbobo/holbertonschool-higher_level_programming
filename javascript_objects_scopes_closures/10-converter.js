c9603b2743e5e38e03c7067961aae42c91062564
root <casonsbobo@gmail.com>
2023-08-01 11:34:55 -0700

    dear god i cant spell

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    return function convertToBaseN (num) {
-        return (num.toString(base));
-    };
+  return function convertToBaseN (num) {
+    return (num.toString(base));
+  };
 };
--- javascript_objects_scopes_closures/7-occurences.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-  exports.nbOccurences = function (list, searchElement) {
-    const occurrences = {};
-    for (const item of list) {
-      occurrences[item] = occurrences[item] ? occurrences[item] + 1 : 1;
-    }
-    return occurrences;
-  }
- 
