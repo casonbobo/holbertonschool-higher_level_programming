d106d2fa011fb49cf0ff7de03639e09693e0f619
casonbobo <casonsbobo@gmail.com>
2023-08-03 20:27:51 +0000

    this is going so slow

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const website = 'https://swapi-api.hbtn.io/api/films/' + url;
 
-request(website, function (err, response, data) {
+request(url, function (err, response, data) {
   if (err) {
     console.error(err);
   } else {
