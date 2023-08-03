887f26c0c30ac9e07b63801ade8ad064672c29ea
casonbobo <casonsbobo@gmail.com>
2023-08-03 18:39:10 +0000

    UGH

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 function printFileContents (filePath) {
-  fs.writeFile(filePath, 'utf8', (err) => {
+  fs.writeFile(filePath, (err) => {
     if (err) {
       console.error('Error:', err);
     } else {
