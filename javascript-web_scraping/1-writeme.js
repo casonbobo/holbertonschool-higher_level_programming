4b8be05746a4d69907e2e37a3f3059a7d850c35c
casonbobo <casonsbobo@gmail.com>
2023-08-03 18:59:41 +0000

    maybe

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-function printFileContents (filePath) {
-  fs.writeFile(filePath, content, 'utf-8', (err, content) => {
+  fs.writeFile(filePath, content, 'utf-8', function (err, content) {
     if (err) {
       console.error(err);
     }
   });
-}
