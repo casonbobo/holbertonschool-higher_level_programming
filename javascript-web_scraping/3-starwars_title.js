1659455bc5a2c70a328e5711652bcd2e9b684000
casonbobo <casonsbobo@gmail.com>
2023-08-03 20:02:43 +0000

    waaahhhhh

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const api_url = 'https://swapi-api.hbtn.io/api/films/' + url;
+const website = 'https://swapi-api.hbtn.io/api/films/' + url;
 
 request(website, function (err, response, data) {
   if (err) {
